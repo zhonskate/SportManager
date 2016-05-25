d51f5cab09fe742d6f1eaf798b2e141078d291de
Ramon Ruiz Dolz <raruidol@inf.upv.es>
2016-05-25 16:54:58 +0200

    Ver 0.9

--- src/controller/ChartsController.java
+++ src/controller/ChartsController.java
@@ -7,12 +7,18 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.TextField;
+import jgpx.model.analysis.Chunk;
+import jgpx.model.analysis.TrackData;
 
 /**
  * FXML Controller class
@@ -22,28 +28,90 @@ import javafx.scene.control.TextField;
 public class ChartsController implements Initializable {
 
     @FXML
-    private LineChart<?, ?> HeightChart;
+    private AreaChart<Number, Number> HeightChart;
     @FXML
-    private LineChart<?, ?> SpeedChart;
+    private LineChart<Number, Number> SpeedChart;
     @FXML
-    private LineChart<?, ?> HeartRateChart;
+    private LineChart<Number, Number> HeartRateChart;
     @FXML
-    private LineChart<?, ?> PedalingChart;
+    private LineChart<Number, Number> PedalingChart;
     @FXML
     private PieChart pieChart;
     @FXML
     private TextField HRField;
-
+    public XYChart.Series<Number, Number> seriesA;
+    public XYChart.Series<Number, Number> seriesB;
+    public XYChart.Series<Number, Number> seriesC;
+    public XYChart.Series<Number, Number> seriesD;
+    public XYChart.Series<Number, Number> seriesAt;
+    public XYChart.Series<Number, Number> seriesBt;
+    public XYChart.Series<Number, Number> seriesCt;
+    public XYChart.Series<Number, Number> seriesDt;
+    private TrackData trackData;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    }
 
     @FXML
     private void onSubmit(ActionEvent event) {
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+        int maxhr;
+        try{maxhr = Integer.parseInt( HRField.getText());}
+        catch(NumberFormatException e){ maxhr = 0;}
+        
+        ObservableList<Chunk> chunks = trackData.getChunks();
+
+        int z1 = 0;
+        int z2 = 0;
+        int z3 = 0;
+        int z4 = 0;
+        int z5 = 0;
+        for (int i = 0; i < chunks.size(); i++) {
+            int gethr = (int) chunks.get(i).getAvgHeartRate();
+
+            if (gethr < maxhr * 0.6) {
+                z1++;
+            } else if (gethr >= maxhr * 0.6 && gethr < maxhr * 0.7) {
+                z2++;
+            } else if (gethr >= maxhr * 0.7 && gethr < maxhr * 0.8) {
+                z3++;
+            } else if (gethr >= maxhr * 0.8 && gethr < maxhr * 0.9) {
+                z4++;
+            } else {
+                z5++;
+            }
+        
+        
+        
     }
-    
+        int totals = z1+z2+z3+z4+z5;
+        
+        pieChartData.add(new PieChart.Data("Recovery zone " + z1*100/totals + "%", z1));
+        pieChartData.add(new PieChart.Data("Endurance zone "+ z2*100/totals + "%", z2));
+        pieChartData.add(new PieChart.Data("Tempo zone "+ z3*100/totals + "%", z3));
+        pieChartData.add(new PieChart.Data("Threshold zhone "+ z4*100/totals + "%", z4));
+        pieChartData.add(new PieChart.Data("Anaerobic zone "+ z5*100/totals + "%", z5));
+
+        pieChart.setData(pieChartData);
+    }
+
+    void setSeries(TrackData trackData,XYChart.Series<Number, Number> seriesA, XYChart.Series<Number, Number> seriesB, XYChart.Series<Number, Number> seriesC, XYChart.Series<Number, Number> seriesD) {
+        this.trackData = trackData;
+        this.seriesA = seriesA;
+        this.seriesB = seriesB;
+        this.seriesC = seriesC;
+        this.seriesD = seriesD;
+
+        HeightChart.getData().add(seriesA);
+        SpeedChart.getData().add(seriesB);
+        HeartRateChart.getData().add(seriesC);
+        PedalingChart.getData().add(seriesD);
+
+        System.out.print("8--D");
+
+    }
 }
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -22,10 +22,10 @@ import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.media.Track;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javax.xml.bind.JAXBContext;
@@ -34,6 +34,7 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import jgpx.model.analysis.Chunk;
 import jgpx.model.analysis.TrackData;
+import jgpx.model.gpx.Track;
 import jgpx.model.jaxb.GpxType;
 import jgpx.model.jaxb.TrackPointExtensionT;
 import jgpx.util.DateTimeUtils;
@@ -79,6 +80,15 @@ public class MainController implements Initializable {
     @FXML
     private Button chartButt;
 
+    public XYChart.Series<Number, Number> seriesA;
+    public XYChart.Series<Number, Number> seriesB;
+    public XYChart.Series<Number, Number> seriesC;
+    public XYChart.Series<Number, Number> seriesD;
+    public XYChart.Series<Number, Number> seriesAt;
+    public XYChart.Series<Number, Number> seriesBt;
+    public XYChart.Series<Number, Number> seriesCt;
+    public XYChart.Series<Number, Number> seriesDt;
+
     /**
      * Initializes the controller class.
      */
@@ -97,80 +107,158 @@ public class MainController implements Initializable {
         AccSlopeUpLbl.setText("0 m");
         AccSlopeDownLbl.setText("0 m");
         MaxPedalRateLbl.setText("0 ppm");
-        AvgPedalRateLbl.setText("0 ppm");   
+        AvgPedalRateLbl.setText("0 ppm");
         chartButt.setDisable(true);
-    }    
+
+    }
 
     @FXML
     private void onLoad(ActionEvent event) throws JAXBException {
-        
+
         FileChooser fileChooser = new FileChooser();
         File file = fileChooser.showOpenDialog(loadButt.getScene().getWindow());
-        if (file == null) {return;}
-        // label.setText("Loading " + file.getName());
-        JAXBContext jaxbContext = JAXBContext.newInstance(GpxType.class, TrackPointExtensionT.class);
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        JAXBElement<Object> root = (JAXBElement<Object>) unmarshaller.unmarshal(file);
-        GpxType gpx = (GpxType) root.getValue();
+        if (file == null) {
+            return;
+        }
 
-        if (gpx != null) {
-            trackData = new TrackData(new jgpx.model.gpx.Track(gpx.getTrk().get(0)));
-            showTrackInfo(trackData);
-            chartButt.setDisable(false);
-            //label.setText("GPX successfully loaded");
-        } else {
-            //label.setText("Error loading GPX from " + file.getName());
-        
-    }
+        Task<Long> task = new Task<Long>() {
+            final Scene _scene = loadButt.getScene();
+
+            @Override
+            protected Long call() throws Exception {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        _scene.setCursor(Cursor.WAIT);
+                    }
+                });
+                long f = 1;
+
+                JAXBContext jaxbContext = JAXBContext.newInstance(GpxType.class, TrackPointExtensionT.class);
+                Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+                JAXBElement<Object> root;
+
+                root = (JAXBElement<Object>) unmarshaller.unmarshal(file);
+
+                GpxType gpx = (GpxType) root.getValue();
+
+                trackData = new TrackData(new Track(gpx.getTrk().get(0)));
+
+                //series
+                ObservableList<Chunk> chunks = trackData.getChunks();
+                seriesA = new XYChart.Series();
+                seriesB = new XYChart.Series();
+                seriesC = new XYChart.Series();
+                seriesD = new XYChart.Series();
+                seriesAt = new XYChart.Series();
+                seriesBt = new XYChart.Series();
+                seriesCt = new XYChart.Series();
+                seriesDt = new XYChart.Series();
+                double acumulated = 0;
+                double time = 0;
+                System.out.println(chunks.size());
+
+                for (int i = 0; i < chunks.size(); i++) {
+
+                    Number xd = ((chunks.get(i).getDistance() + acumulated));
+                    time = chunks.get(i).getDuration().toMillis() + time;
+
+                    if (i % 3 == 0) {
+
+                        seriesA.getData().add(new XYChart.Data<>(xd, chunks.get(i).getAvgHeight()));
+                        seriesB.getData().add(new XYChart.Data<>(xd, chunks.get(i).getSpeed()));
+                        seriesC.getData().add(new XYChart.Data<>(xd, chunks.get(i).getAvgHeartRate()));
+                        //if(i%15==0)
+                        seriesD.getData().add(new XYChart.Data<>(xd, chunks.get(i).getAvgCadence()));
+                    }
+                    if (true) {
+
+                        String timexd = String.format("%.0fm", time / 60000);
+                        //   System.out.println(timexd);
+                        seriesAt.getData().add(new XYChart.Data<>(time, chunks.get(i).getAvgHeight()));
+                        seriesBt.getData().add(new XYChart.Data<>(time, chunks.get(i).getSpeed()));
+                        seriesCt.getData().add(new XYChart.Data<>(time, chunks.get(i).getAvgHeartRate()));
+                        seriesDt.getData().add(new XYChart.Data<>(time, chunks.get(i).getAvgCadence()));
+                    }
+
+                    acumulated += chunks.get(i).getDistance();
+
+                }
+                Platform.runLater(new Runnable() {
+                @Override public void run() {
+                _scene.setCursor(Cursor.DEFAULT);
+                }});
+                return f;
+            }
+
+        };
+        task.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent event) {
+                try {
+                    chartButt.setDisable(false);
+                    showTrackInfo(trackData);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Problem with the file");
+                    alert.setContentText("There was a problem with the file, choose a correct one.");
+                    alert.showAndWait();
+                }
+            }
+        });
+
+        Thread th = new Thread(task);
+        th.setDaemon(true);
+        th.start();
+
     }
 
     @FXML
-   
+
     private void onChart(ActionEvent event) {
-        try{
+        try {
             FXMLLoader charger = new FXMLLoader(getClass().getResource("/view/charts.fxml"));
-                Parent root = charger.load();   
-                Scene scene = new Scene(root);
-            
-                Stage chz = new Stage();
-                chz.setTitle("Charts");
-                chz.setScene(scene);
-                chz.setResizable(true);
-                chz.setMinHeight(600.0);
-                chz.setMinWidth(750.0);
-                chz.show();
+            Parent root = charger.load();
+            Scene scene = new Scene(root);
+            ((ChartsController) charger.getController()).setSeries(trackData, seriesA, seriesB, seriesC, seriesD);
+            Stage chz = new Stage();
+            chz.setTitle("Charts");
+            chz.setScene(scene);
+            chz.setResizable(true);
+            chz.setMinHeight(600.0);
+            chz.setMinWidth(750.0);
+            chz.show();
+        } catch (IOException e) {
         }
-        catch (IOException e){}
     }
 
     @FXML
     private void onExit(ActionEvent event) {
-        ((Stage) ((Node) event.getSource()).getScene().getWindow()).close(); 
+        ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
     }
-    
-     private void showTrackInfo(TrackData trackData) {
+
+    private void showTrackInfo(TrackData trackData) {
         startDateLbl.setText(DateTimeUtils.format(trackData.getStartTime()).substring(0, 10));
         StartTimeLbl.setText(DateTimeUtils.format(trackData.getStartTime()).substring(11));
-        TotalDistanceLbl.setText(String.format("%.0f m",trackData.getTotalDistance()));
-        DurationLbl.setText(DateTimeUtils.format(trackData.getTotalDuration()).substring(0,2)+"h "+
-                DateTimeUtils.format(trackData.getTotalDuration()).substring(3,5)+"m "+
-                DateTimeUtils.format(trackData.getTotalDuration()).substring(6,8)+"s");
-        ExerciseTimeLnl.setText(DateTimeUtils.format(trackData.getMovingTime()).substring(0,2)+"h "+
-                DateTimeUtils.format(trackData.getMovingTime()).substring(3,5)+"m "+
-                DateTimeUtils.format(trackData.getMovingTime()).substring(6,8)+"s");
+        TotalDistanceLbl.setText(String.format("%.0f m", trackData.getTotalDistance()));
+        DurationLbl.setText(DateTimeUtils.format(trackData.getTotalDuration()).substring(0, 2) + "h "
+                + DateTimeUtils.format(trackData.getTotalDuration()).substring(3, 5) + "m "
+                + DateTimeUtils.format(trackData.getTotalDuration()).substring(6, 8) + "s");
+        ExerciseTimeLnl.setText(DateTimeUtils.format(trackData.getMovingTime()).substring(0, 2) + "h "
+                + DateTimeUtils.format(trackData.getMovingTime()).substring(3, 5) + "m "
+                + DateTimeUtils.format(trackData.getMovingTime()).substring(6, 8) + "s");
         MaxHrLbl.setText(trackData.getMaxHeartrate() + " bpm");
         AvgHrLbl.setText(trackData.getAverageHeartrate() + " bpm");
         MinHrLbl.setText(trackData.getMinHeartRate() + " bpm");
-        MaxSpeedLbl.setText(String.format("%.2f km/h", (trackData.getMaxSpeed()*3.6)));
-        AvgSpeedLbl.setText(String.format("%.2f km/h", (trackData.getAverageSpeed()*3.6)));
+        MaxSpeedLbl.setText(String.format("%.2f km/h", (trackData.getMaxSpeed() * 3.6)));
+        AvgSpeedLbl.setText(String.format("%.2f km/h", (trackData.getAverageSpeed() * 3.6)));
         AccSlopeUpLbl.setText(String.format("%.2f m", trackData.getTotalAscent()));
         AccSlopeDownLbl.setText(String.format("%.2f m", trackData.getTotalDescend()));
         MaxPedalRateLbl.setText(String.format("%d ppm", trackData.getMaxCadence()));
-        AvgPedalRateLbl.setText(String.format("%d ppm", trackData.getAverageCadence()));      
-          
+        AvgPedalRateLbl.setText(String.format("%d ppm", trackData.getAverageCadence()));
+
     }
-     
-   
-    
-    
+
 }
